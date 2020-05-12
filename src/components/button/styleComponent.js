fa27f2520679baac2f4d0697ba6307d3aa915bf1
MusaW <musa@wizardsdev.com>
2020-05-12 21:26:03 +0300

    GM-01-addet-logic-Login-Page

--- src/components/button/styleComponent.js
+++ src/components/button/styleComponent.js
@@ -3,14 +3,17 @@ import styled from "styled-components";
 const bgInput = '#4C83FF';
 const WrapperBgColor = '#32CCBC';
 
+const btnHeight = 60;
+
 export const Wrapper = styled.button`
-    font-size: 20px;
-    padding: 20px;
+    height: ${btnHeight}px;
     margin: 20px;
-    background-image: linear-gradient(90deg, ${WrapperBgColor} 0, ${WrapperBgColor} 50%, ${bgInput} 50%, ${bgInput} 100%);
     border: none;
+    padding: 10px;
+    font-size: 20px;
     border-radius: 3px;
     background-size: 200%;
+    background-image: linear-gradient(90deg, ${WrapperBgColor} 0, ${WrapperBgColor} 50%, ${bgInput} 50%, ${bgInput} 100%);
     background-position-x: 100%;
     transition: background .2s ease-out;
     
--- src/components/input/styleComponents.js
+++ src/components/input/styleComponents.js
@@ -3,15 +3,16 @@ import styled from "styled-components";
 const bgInput = '#4C83FF';
 const colorPlaceholder = '#000';
 const WrapperBgColor = '#32CCBC';
+const inputHeight = 40;
 
 export const Wrapper = styled.input`
-    font-size: 18px;
-    padding: 20px;
+    height: ${inputHeight}px;
     margin: 20px;
     border: none;
+    padding: 10px; 
+    font-size: 18px;
     border-radius: 10px;
     background-image: linear-gradient(90deg, ${WrapperBgColor} 0, ${WrapperBgColor} 50%, ${bgInput} 50%, ${bgInput} 100%);
-    border: none;
     border-radius: 3px;
     background-size: 200%;
     background-position-x: 100%;
